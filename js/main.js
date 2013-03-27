4eafd1cf330ac9e4dfaff0d261849187fc3d5507
Brian Drelling <brian.drelling@gmail.com>
2013-03-26 23:29:45 -0500

    Small changes.

--- js/main.js
+++ js/main.js
@@ -1,55 +1,10 @@
 $(document).ready(function() {
-
-    var people = ['Brian Drelling', 'Brian Lewis', 'Daniel Bearden', 'Devin Collins', 'Eric Stanley', 'Milly Carter'];
     
     var restaurants;
     
-    $.getJSON('json/restaurants.json', function(data) {
-        restaurants = data;
-        console.log(restaurants);
-    });
-    
-    $(window).load(function() {
-        populateSelects();
-        getRestaurant();
-    });
-    
-    $('.people').on('click', 'a', function() {
-        $(this).closest('li').toggleClass('active');
-    });
-
-    function populateSelects() {
-        for (i in people) {
-            $('.people').append('<li class="active"><a>' + people[i] + '</a></li>');
-        }
-        for (i in restaurants) {
-            $('.restaurants').append('<option value="' + i + '">' + restaurants[i].name + '</option>');
-        }
-    }
-    
-    function getRestaurant() {
-        var random;
-        var selectedRestaurant;
-        var selectedPeople = getSelectedPeople()
-        
-        selectedRestaurant = restaurants[random];
-        
-        do {
-            random = Math.floor(restaurants.length * (Math.random() % 1));
-            selectedRestaurant = restaurants[random];
-            console.log(selectedRestaurant);
-        } while(isRestaurantAcceptable(selectedRestaurant, selectedPeople) == false);
-        
-        $(".restaurants > option").attr('selected',false).eq(random).attr('selected',true);
-    }
-    
-    function isRestaurantAcceptable(restaurant, people) {
-        for (i in people) {
-            if (restaurant.dislikes.indexOf(people[i]) !== -1) {
-                return false;
-            }
-        }
-        return true;
+    if(navigator.geolocation)
+    {
+        navigator.geolocation.getCurrentPosition(getVenue);
     }
     
 
@@ -64,21 +19,34 @@ $(document).ready(function() {
         
         return people;
     }
-
-    var url = 'https://api.foursquare.com/v2/venues/explore';
-    var oauth = 'D53VJTZAO1P0OYL0JNLTV3HDI21IMKUTSQXL1BCYFBVWUCVK';
-    var apiVersion = '20130326';
     
-    function getVenues() {
+    function getVenue(position) {
+        
+        var ll = position.coords.latitude + ',' + position.coords.longitude;
+
+        var apiVersion = '20130326';
+        var oauth = 'D53VJTZAO1P0OYL0JNLTV3HDI21IMKUTSQXL1BCYFBVWUCVK';
+        var section = 'food';
+        var url = 'https://api.foursquare.com/v2/venues/explore';
+        
         $.ajax({
             data: { 
-                ll: '40.7,-74', 
+                limit: 50,
+                //near: 'Minneapolis, MN',
+                ll: ll,
                 oauth_token: oauth, 
+                section: section,
                 v: apiVersion 
             },
             dataType: 'json',
             success: function(data) {
-                console.log(data);
+                console.log(data.response.groups[0].items);
+                console.log(data.response.groups[0].items[0].venue.name);
+                
+                
+                var venues = data.response.groups[0].items;
+                
+                $('.venue').text(venues[0].venue.name);
             },
             url: url
         });
