36dee8a614be8a14488a44f3e01f77cdb8ecfa1e
Brian Drelling <brian.drelling@gmail.com>
2013-03-27 23:09:17 -0500

    Made it work.

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+var venues;
+    
 $(document).ready(function() {
     
     var restaurants;
@@ -7,17 +9,17 @@ $(document).ready(function() {
         navigator.geolocation.getCurrentPosition(getVenue);
     }
     
-
-});
+    $('.btn[data-action="skip"]').click(function() {
+        skipVenue();
+    });
     
-    function getSelectedPeople() {
-        var people = [];
-        
-        $('.people li.active a').each(function() {
-            people.push($(this).text());
-        });
-        
-        return people;
+    function skipVenue() {
+        venues.shift();
+        if (venues.length > 0) {
+            $('.venue').text(venues[0].venue.name);
+        } else {
+            $('.venue').text("Fine. Just don't fucking eat then.");
+        }
     }
     
     function getVenue(position) {
@@ -25,13 +27,14 @@ $(document).ready(function() {
         var ll = position.coords.latitude + ',' + position.coords.longitude;
 
         var apiVersion = '20130326';
+        var limit = 50;
         var oauth = 'D53VJTZAO1P0OYL0JNLTV3HDI21IMKUTSQXL1BCYFBVWUCVK';
         var section = 'food';
         var url = 'https://api.foursquare.com/v2/venues/explore';
         
         $.ajax({
             data: { 
-                limit: 50,
+                limit: limit,
                 //near: 'Minneapolis, MN',
                 ll: ll,
                 oauth_token: oauth, 
@@ -40,14 +43,25 @@ $(document).ready(function() {
             },
             dataType: 'json',
             success: function(data) {
-                console.log(data.response.groups[0].items);
-                console.log(data.response.groups[0].items[0].venue.name);
-                
-                
-                var venues = data.response.groups[0].items;
-                
+                venues = data.response.groups[0].items;
+                venues.shuffle();
                 $('.venue').text(venues[0].venue.name);
             },
             url: url
         });
     }
+
+    Array.prototype.shuffle = function() {
+      var i = this.length, j, tempi, tempj;
+      if ( i == 0 ) return this;
+      while ( --i ) {
+         j       = Math.floor( Math.random() * ( i + 1 ) );
+         tempi   = this[i];
+         tempj   = this[j];
+         this[i] = tempj;
+         this[j] = tempi;
+      }
+      return this;
+    }
+
+});
